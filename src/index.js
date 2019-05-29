9f121e7cb305b20390810bdcbe0a71c112aeca9a
Mark Beale <Mark.Beale@sgs.com>
2019-05-29 14:07:00 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+let canvas = document.getElementById("gameScreen");
+
+let ctx = canvas.getContext("2d");
+
+ctx.fillStyle = '#f000';
+ctx.fillRect(20, 20, 100, 100);
+
+ctx.fillStyle = '#';
+ctx.fillRect(200, 200, 50, 50);
+
+
